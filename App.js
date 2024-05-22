0b81d54120cb397361741b7829cbc8965b5aaf58
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2024-05-22 23:27:20 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,22 @@
 
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Provider as PaperProvider } from 'react-native-paper';
+import HomeScreen from './screens/HomeScreen';
+import AddExpenseScreen from './screens/AddExpenseScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Expense Tracker' }} />
+          <Stack.Screen name="AddExpense" component={AddExpenseScreen} options={{ title: 'Add Expense' }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  );
+}
