c256c3642c412a4a4a54d384ec2e63b299cd1000
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-01-23 23:01:44 +0530

    Update AddExpenseForm.js

--- AddExpenseForm.js
+++ AddExpenseForm.js
@@ -54,3 +54,4 @@ setFormData({
     <div style={{ marginBottom: '2rem' }}>
       <h2>{isEditing ? 'Edit Expense' : 'Add New Expense'}</h2>
       <form onSubmit={handleSubmit} style={{ display: 'flex', gap: '1rem' }}>
+        <input
