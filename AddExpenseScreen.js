54d95a96a538341c27a727a24673ebd1fa1c3156
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-02-18 21:53:25 +0530

    Update AddExpenseScreen.js

--- AddExpenseScreen.js
+++ AddExpenseScreen.js
@@ -102,3 +102,4 @@ const AddExpenseScreen = ({ navigation, route }) => {
         />
         {amountError && (
           <HelperText type="error" visible={amountError}>
+            Please enter a valid amount greater than 0.
