363bfb54d7377d11de8bb146c3965023a41376ef
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-03-08 23:26:31 +0530

    Update AddExpenseForm.js

--- AddExpenseForm.js
+++ AddExpenseForm.js
@@ -4,3 +4,4 @@ function AddExpenseForm({ editingExpense, onCancelEdit }) {
   const { addExpense, editExpense } = useExpenses();
   const isEditing = Boolean(editingExpense);
   const [formData, setFormData] = useState({
+    id: '',
