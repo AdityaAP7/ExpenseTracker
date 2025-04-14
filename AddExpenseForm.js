edc89face19bb062c749f3d15d08ba44ae2f05d0
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-04-14 23:55:17 +0530

    Update AddExpenseForm.js

--- AddExpenseForm.js
+++ AddExpenseForm.js
@@ -2,3 +2,4 @@
 import React, { useState, useEffect } from 'react';
 import { useExpenses } from '../path-to-useExpenses';
 function AddExpenseForm({ editingExpense, onCancelEdit }) {
+  const { addExpense, editExpense } = useExpenses();
