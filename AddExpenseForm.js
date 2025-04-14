a5f5bd829ee0ad48ad72f1e036e501fe088ee41b
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-04-14 23:58:24 +0530

    Update AddExpenseForm.js

--- AddExpenseForm.js
+++ AddExpenseForm.js
@@ -18,3 +18,4 @@ function AddExpenseForm({ editingExpense, onCancelEdit }) {
         description: editingExpense.description,
        category: editingExpense.category,
         amount: editingExpense.amount,
+        date: editingExpense.date,
