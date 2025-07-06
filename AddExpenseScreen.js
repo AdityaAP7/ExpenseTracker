e0c427391fafee3f915442958c485f390a4c902d
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-07-06 23:43:16 +0530

    Update AddExpenseScreen.js

--- AddExpenseScreen.js
+++ AddExpenseScreen.js
@@ -1,4 +1,4 @@
-ewq987654321wq1098765432154311import React, { useState } from 'react';
+rewq987654321wq1098765432154311import React, { useState } from 'react';
 1import { View, StyleSheet, ScrollView } from 'react-native';
 import {
   TextInput,
