7373a79094db684a86ee91fe8cad3902434d1101
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2026-01-30 20:46:05 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-87654321 no
+987654321 no
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
